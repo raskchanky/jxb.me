0523a8fbe65e15986684f4fa475f7d2561634a88
Josh Black <raskchanky@gmail.com>
2010-11-17 21:02:40 -0800

    bug fixes, adding some basic html views

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,14 +1,41 @@
 class SitesController < ApplicationController
-  def show
+  def go
     @site = Site.find_with_url(params[:url])
+
+    if @site
+      redirect_to @site.url
+    else
+      respond_to do |format|
+        format.html { render :action => 'not_found' }
+      end
+    end
+  end
+
+  def not_found
+    respond_to do |format|
+      format.html
+      format.json { render :text => 'URL not found', :status => 404 }
+    end
+  end
+
+  def show
+    @site = Site.find_with_url(params[:id])
 
     respond_to do |format|
       if @site
-        format.html { redirect_to @site.url }
-        format.json { render :json => @site.url }
+        format.html
+        format.json { render :json => @site, :only => [:url, :short_url, :long_url] }
       else
-        format.json { render :text => '', :status => :not_found }
+        format.json { render :action => 'not_found' }
       end
+    end
+  end
+
+  def new
+    @site = Site.new
+
+    respond_to do |format|
+      format.html
     end
   end
 
@@ -17,8 +44,10 @@ class SitesController < ApplicationController
 
     respond_to do |format|
       if @site.save
-        format.json { render :json => short_url(@site), :status => :created }
+        format.html { redirect_to @site }
+        format.json { render :json => {:short_url => shortened_url(@site.short_url), :long_url => lengthened_url(@site.long_url)}, :status => :created }
       else
+        format.html { render :action => 'new' }
         format.json { render :json => @site.errors, :status => :unprocessable_entity }
       end
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,62 +1,6 @@
 JxbMe::Application.routes.draw do
-  resources :sites, :only => [:create]
-  match '/:url' => 'sites#show', :url => /[a-z0-9]{1,6}/, :as => :short
-  match '/:url' => 'sites#show', :url => /[0-1]+/, :as => :long
-
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
+  resources :sites, :only => [:create, :show, :new]
+  match '/:url' => 'sites#go', :url => /[a-zA-Z0-9]{1,6}/, :as => :shortened
+  match '/:url' => 'sites#go', :url => /[0-1]+/, :as => :lengthened
+  root :to => 'sites#new'
 end
